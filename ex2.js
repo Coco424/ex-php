617cf85569e2d62e65fb3d5cda5cdd236296e650
Corinne COUPIER <coupier.corinne@gmail.com>
2018-12-02 19:53:43 +0100

    ajout ex2

--- ex2.js
+++ ex2.js
@@ -1,13 +1,29 @@
 $(function(){
-    function secondes(jour, heures, minutes){
-        return jour*3600*24 + heures*3600 + minutes*60;
-    }
-    
-    
+
+function perimetre(l,h){
+    return (l+h)*2;
+}
+
+function aire(l,h){
+    return l*h;
+}
+
 $('#calculer').click(function(){
-    let jour=parseInt($('#jour').val());
-    let heures=parseInt($('#heures').val());
-    let minutes=parseInt($('#minutes').val());
-    $('p').text("Secondes : " + secondes(jour, heures, minutes));
-    })
+    let largeur=parseInt($('#largeur').val());
+    let longueur=parseInt($('#longueur').val());
+    $('p').text("Périmètre : " + perimetre(largeur,longueur)+" / Aire : "+aire(largeur,longueur));
 })
+})
+
+function conversion (jours, heures, minutes){
+
+return (jours*24*60*60) + (heures*60*60) + (minutes*60);
+}
+
+$('#conversion').click(function(){
+    let jours = parseInt($('#jours').val());
+    let heures = parseInt($('#heures').val());
+    let minutes = parseInt($('#minutes').val());
+    $('#p2').text("Temps en secondes : " + conversion(jours, heures, minutes));
+})
+
